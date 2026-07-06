3539f74de3f31bad3e448e1c378eec5eeed845b5
pmeir99 <145489785+pmeir99@users.noreply.github.com>
2026-07-06 17:47:42 -0400

    Refactor build script for improved option handling

--- build.sh
+++ build.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-LONG=sideloaded,rootless,trollstore
-OPTS=$(getopt -a --longoptions "$LONG" -- "$@")
-
-if [ $? -ne 0 ]; then
-  echo -e '\033[1m\033[31mInvalid build option.\033[0m'
-  exit 1
-fi
-
-eval set -- "$OPTS"
-
 set_plist_string() {
   local plist_path="$1"
   local key="$2"
@@ -84,113 +74,118 @@ patch_ipa_privacy_strings() {
   return 0
 }
 
-while :; do
-  case "$1" in
-    --sideloaded)
-      echo -e '\033[1m\033[32mBuilding BHTwitter project for sideloaded.\033[0m'
+BUILD_MODE="${1:-rootfull}"
 
-      make clean
-      rm -rf .theos
-      make SIDELOADED=1
+case "$BUILD_MODE" in
+  --sideloaded|sideloaded)
+    echo -e '\033[1m\033[32mBuilding BHTwitter project for sideloaded.\033[0m'
 
-      if [ $? -eq 0 ]; then
-        echo -e '\033[1m\033[32mMake command succeeded.\033[0m'
-      else
-        echo -e '\033[1m\033[31mMake command failed.\033[0m'
-        exit 1
-      fi
+    make clean
+    rm -rf .theos
+    make SIDELOADED=1
 
-      if [ -e ./packages/com.atebits.Tweetie2.ipa ]; then
-        echo -e '\033[1m\033[32mPatching source IPA before injection.\033[0m'
-
-        patch_ipa_privacy_strings \
-          "packages/com.atebits.Tweetie2.ipa" \
-          "packages/com.atebits.Tweetie2.privacy.ipa"
-
-        if [ $? -ne 0 ]; then
-          exit 1
-        fi
-
-        echo -e '\033[1m\033[32mBuilding the IPA.\033[0m'
-
-        cyan -i packages/com.atebits.Tweetie2.privacy.ipa -o packages/BHTwitter-sideloaded --ignore-encrypted \
-          -uwf .theos/obj/debug/keychainfix.dylib .theos/obj/debug/BHTwitter.dylib layout/Library/Application\ Support/BHT/BHTwitter.bundle
-
-        echo -e '\033[1m\033[32mDone, thanks for using BHTwitter.\033[0m'
-      else
-        echo -e '\033[1m\033[0;31mpackages/com.atebits.Tweetie2.ipa not found.\033[0m'
-      fi
-
-      break
-      ;;
-
-    --rootless)
-      echo -e '\033[1m\033[32mBuilding BHTwitter project for Rootless.\033[0m'
-
-      make clean
-      rm -rf .theos
-      export THEOS_PACKAGE_SCHEME=rootless
-      make package
-
-      echo -e '\033[1m\033[32mDone, thanks for using BHTwitter.\033[0m'
-      break
-      ;;
-
-    --trollstore)
-      echo -e '\033[1m\033[32mBuilding BHTwitter project for TrollStore.\033[0m'
-
-      make clean
-      rm -rf .theos
-      make
-
-      if [ $? -eq 0 ]; then
-        echo -e '\033[1m\033[32mMake command succeeded.\033[0m'
-      else
-        echo -e '\033[1m\033[31mMake command failed.\033[0m'
-        exit 1
-      fi
-
-      if [ -e ./packages/com.atebits.Tweetie2.ipa ]; then
-        echo -e '\033[1m\033[32mPatching source IPA before injection.\033[0m'
-
-        patch_ipa_privacy_strings \
-          "packages/com.atebits.Tweetie2.ipa" \
-          "packages/com.atebits.Tweetie2.privacy.ipa"
-
-        if [ $? -ne 0 ]; then
-          exit 1
-        fi
-
-        echo -e '\033[1m\033[32mBuilding the IPA.\033[0m'
-
-        cyan -i packages/com.atebits.Tweetie2.privacy.ipa -o packages/BHTwitter-trollstore.tipa --ignore-encrypted \
-          -uwf .theos/obj/debug/BHTwitter.dylib layout/Library/Application\ Support/BHT/BHTwitter.bundle
-
-        echo -e '\033[1m\033[32mDone, thanks for using BHTwitter.\033[0m'
-      else
-        echo -e '\033[1m\033[0;31mpackages/com.atebits.Tweetie2.ipa not found.\033[0m'
-      fi
-
-      break
-      ;;
-
-    --)
-      shift
-
-      echo -e '\033[1m\033[32mBuilding BHTwitter project for Rootfull.\033[0m'
-
-      make clean
-      rm -rf .theos
-      unset THEOS_PACKAGE_SCHEME
-      make package
-
-      echo -e '\033[1m\033[32mDone, thanks for using BHTwitter.\033[0m'
-      break
-      ;;
-
-    *)
-      echo -e '\033[1m\033[31mUnknown option.\033[0m'
+    if [ $? -eq 0 ]; then
+      echo -e '\033[1m\033[32mMake command succeeded.\033[0m'
+    else
+      echo -e '\033[1m\033[31mMake command failed.\033[0m'
       exit 1
-      ;;
-  esac
-done
+    fi
+
+    if [ -e ./packages/com.atebits.Tweetie2.ipa ]; then
+      echo -e '\033[1m\033[32mPatching source IPA before injection.\033[0m'
+
+      patch_ipa_privacy_strings \
+        "packages/com.atebits.Tweetie2.ipa" \
+        "packages/com.atebits.Tweetie2.privacy.ipa"
+
+      if [ $? -ne 0 ]; then
+        exit 1
+      fi
+
+      echo -e '\033[1m\033[32mBuilding the IPA.\033[0m'
+
+      cyan -i packages/com.atebits.Tweetie2.privacy.ipa -o packages/BHTwitter-sideloaded --ignore-encrypted \
+        -uwf .theos/obj/debug/keychainfix.dylib .theos/obj/debug/BHTwitter.dylib layout/Library/Application\ Support/BHT/BHTwitter.bundle
+
+      echo -e '\033[1m\033[32mDone, thanks for using BHTwitter.\033[0m'
+    else
+      echo -e '\033[1m\033[0;31mpackages/com.atebits.Tweetie2.ipa not found.\033[0m'
+      exit 1
+    fi
+    ;;
+
+  --rootless|rootless)
+    echo -e '\033[1m\033[32mBuilding BHTwitter project for Rootless.\033[0m'
+
+    make clean
+    rm -rf .theos
+    export THEOS_PACKAGE_SCHEME=rootless
+    make package
+
+    if [ $? -eq 0 ]; then
+      echo -e '\033[1m\033[32mDone, thanks for using BHTwitter.\033[0m'
+    else
+      echo -e '\033[1m\033[31mMake command failed.\033[0m'
+      exit 1
+    fi
+    ;;
+
+  --trollstore|trollstore)
+    echo -e '\033[1m\033[32mBuilding BHTwitter project for TrollStore.\033[0m'
+
+    make clean
+    rm -rf .theos
+    make
+
+    if [ $? -eq 0 ]; then
+      echo -e '\033[1m\033[32mMake command succeeded.\033[0m'
+    else
+      echo -e '\033[1m\033[31mMake command failed.\033[0m'
+      exit 1
+    fi
+
+    if [ -e ./packages/com.atebits.Tweetie2.ipa ]; then
+      echo -e '\033[1m\033[32mPatching source IPA before injection.\033[0m'
+
+      patch_ipa_privacy_strings \
+        "packages/com.atebits.Tweetie2.ipa" \
+        "packages/com.atebits.Tweetie2.privacy.ipa"
+
+      if [ $? -ne 0 ]; then
+        exit 1
+      fi
+
+      echo -e '\033[1m\033[32mBuilding the IPA.\033[0m'
+
+      cyan -i packages/com.atebits.Tweetie2.privacy.ipa -o packages/BHTwitter-trollstore.tipa --ignore-encrypted \
+        -uwf .theos/obj/debug/BHTwitter.dylib layout/Library/Application\ Support/BHT/BHTwitter.bundle
+
+      echo -e '\033[1m\033[32mDone, thanks for using BHTwitter.\033[0m'
+    else
+      echo -e '\033[1m\033[0;31mpackages/com.atebits.Tweetie2.ipa not found.\033[0m'
+      exit 1
+    fi
+    ;;
+
+  --rootfull|rootfull|"")
+    echo -e '\033[1m\033[32mBuilding BHTwitter project for Rootfull.\033[0m'
+
+    make clean
+    rm -rf .theos
+    unset THEOS_PACKAGE_SCHEME
+    make package
+
+    if [ $? -eq 0 ]; then
+      echo -e '\033[1m\033[32mDone, thanks for using BHTwitter.\033[0m'
+    else
+      echo -e '\033[1m\033[31mMake command failed.\033[0m'
+      exit 1
+    fi
+    ;;
+
+  *)
+    echo -e "\033[1m\033[31mUnknown build option: $BUILD_MODE\033[0m"
+    echo "Usage: ./build.sh [--rootfull|--rootless|--sideloaded|--trollstore]"
+    exit 1
+    ;;
+esac
